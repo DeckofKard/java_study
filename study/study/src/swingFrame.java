b8ff491e81097d550abce2a302ddac0f7f754cdf
DeckofKard <qww9363@gmail.com>
2023-06-14 12:09:49 +0900

    swingFrame

---
+++ study/study/src/swingFrame.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+import java.awt.*;
+public class swingFrame extends JFrame{
+    public swingFrame(){
+        setTitle("Frame");
+        setSize(1000, 1000);
+        setVisible(true);
+    }
+}
